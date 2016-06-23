8dfb8d484f904cddd61c55acc38c311a9c3dac9e
陈子云 <472206618@qq.com>
2016-06-23 20:39:46 +0800

    添加服务

---
+++ server.js
@@ -0,0 +1,48 @@
+var http = require("http");
+var url = require("url");
+var fs = require('fs');
+
+function start() {
+    function onRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        //静态资源服务器
+        //fs.readFile(filename,[options],callback);
+        if (pathname === '/') {
+            fs.readFile(__dirname + '/index.html', function(err, file){
+                if (err) throw err;
+                response.write(file, 'binary');
+                response.end();
+            });
+        } else {
+            fs.readFile(__dirname + pathname, 'binary', function(err, file){
+                if (err && err.code === 'ENOENT') {
+                    response.writeHead(404, {'Content-Type': 'text/plain'});
+                    response.write(err + "\n");
+                    response.end();
+                } else if (err) {
+                    response.writeHead(500, {'Content-Type': 'text/plain'});
+                    response.write(err + "\n");
+                    response.end();
+                } else {
+                    var type = pathname.slice(pathname.lastIndexOf('.') + 1);
+                    if (type === 'css') {
+                        response.writeHead(200, {'Content-Type': 'text/css'});
+                    } else if (type === 'png') {
+                        response.writeHead(200, {'Content-Type': 'image/png'});
+                    } else if (type === 'js') {
+                        response.writeHead(200, {'Content-Type': 'text/javascript'});
+                    }
+                    response.write(file, 'binary');
+                    response.end();
+                }
+                
+            });
+        }
+
+        
+    }
+    http.createServer(onRequest).listen(3000);
+    console.log("Server has started.");
+}
+
+start();
